183c1a2a72a8011c5458c5d1467f257aaebbf6e6
George Rodrigues <georgerodrigues@live.com.au>
2022-05-03 09:20:02 -0300

    feat: add permissive hold

--- keyboards/crkbd/keymaps/george124816/config.h
+++ keyboards/crkbd/keymaps/george124816/config.h
@@ -28,6 +28,9 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #define MASTER_RIGHT
 // #define EE_HANDS
 
+#define PERMISSIVE_HOLD
+#define IGNORE_MOD_TAP_INTERRUPT
+
 #define TAPPING_FORCE_HOLD
 #define TAPPING_TERM 175
 
